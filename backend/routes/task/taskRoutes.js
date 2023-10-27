c1fa6d4a7b04689433b6652050b502352a0c486b
PranayRJuturu <115672420+PranayRJuturu@users.noreply.github.com>
2023-10-27 17:00:52 -0400

    Added error responses

--- backend/routes/task/taskRoutes.js
+++ backend/routes/task/taskRoutes.js
@@ -7,7 +7,8 @@ const { Task } = require("../../models/tasks/task");
 
 const taskRouter = express.Router();
 
-
+const allowedUpdates = ["title", "group", "tags", "description", "priority", "status", "assignedTo", "deadLine", "startDate", "createdBy", "createdOn"];
+   
 //Get 
 taskRouter.get("/task", (req, res) => {
       Task.find({}, { __v: 0 })
@@ -27,7 +28,7 @@ taskRouter.post("/task", validateTask,(req, res) => {
 
       const errors = validationResult(req).array().map(error => error.msg);
       if(errors.length>0){
-          res.status(499).json(errors);
+          return res.status(499).json(errors);
       };
 
       newTask
@@ -42,41 +43,52 @@ taskRouter.post("/task", validateTask,(req, res) => {
 //TODO: Add userId Header
 //Get Task By Id
 taskRouter.get("/task/:id",async (req, res) => {
-    
-     await Task.findById(req.params.id, { __v: 0}).then((result) => {
-        taskResponses.sendSuccess(res, messages.SUCCESSFUL, result);
-      }).catch((e) => { 
-        taskResponses.sendError(res, messages.TASK_NOT_FOUND, e);
-      });
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      taskResponses.sendError(res, messages.TASK_NOT_FOUND);
+      return;
+    }
+    taskResponses.sendSuccess(res, messages.SUCCESSFUL, task);
 
 });
 
 //Update Task
-taskRouter.patch("/task/:id", async (req,res)=>{
+taskRouter.patch("/task/:id", async (req, res) => {
+  try {
     const updates = Object.keys(req.body);
-    //need to remove few fields from updates
-    const allowedUpdates = ["title", "group", "tags", "description", "priority", "status", "assignedTo", "deadLine", "startDate", "createdBy", "createdOn"];
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    const isValidOperation = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
     if (!isValidOperation) {
       taskResponses.sendError(res, messages.FORBIDDEN);
       return;
     }
-    await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }).then((result) => {
-        taskResponses.sendSuccess(res, messages.SUCCESSFUL_UPDATE, result);
-      }).catch((e) => { 
-        taskResponses.sendError(res, messages.TASK_NOT_FOUND, e);
-      });
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-})
+    if (!updatedTask) {
+      taskResponses.sendError(res, messages.TASK_NOT_FOUND);
+      return;
+    }
+
+    taskResponses.sendSuccess(res, messages.SUCCESSFUL_UPDATE, updatedTask);
+  } catch (error) {
+    console.error(error); 
+    taskResponses.sendError(res, messages.INTERNAL_SERVER_ERROR); 
+  }
+});
 
 //Delete Task
 taskRouter.delete("/task/:id", async (req,res)=>{ 
 
-    await Task.findByIdAndDelete(req.params.id).then((result) => {
-        taskResponses.sendSuccess(res, messages.SUCCESSFUL_DELETE);
-      }).catch((e) => { 
-        taskResponses.sendError(res, messages.TASK_NOT_FOUND, e);
-      });
+  const task =  await Task.findByIdAndDelete(req.params.id);
+  if(!task){
+    taskResponses.sendError(res, messages.TASK_NOT_FOUND);
+    return;
+  }
+  taskResponses.sendSuccess(res, messages.SUCCESSFUL_DELETE);
 });  
 
 module.exports = taskRouter;    
